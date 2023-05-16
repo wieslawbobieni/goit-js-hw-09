8df855d3c7a939fe5d665dd5a6d44e5ced15e041
wieslawbobieni <bk.bobieni@gmail.com>
2023-05-16 11:13:27 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const bodyBg = document.body;
 
+let intervalId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColor() {
+  const color = getRandomHexColor();
+  bodyBg.style.backgroundColor = color;
+}
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  intervalId = setInterval(changeBackgroundColor, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  startButton.disabled = false;
+  clearInterval(intervalId);
+});
